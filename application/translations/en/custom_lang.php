4f69ddd221373ed1bd95e0f6cb5ca6708b200e0f
Ray Snarski <bibletruth@absolutelyright.org>
2013-07-05 07:36:13 -0500

    Create custom_lang.php
    Oxid v.4.7.6 in application/translations/en create an empty custom_lang.php file. Copy contents from above and paste into new file. Save.

---
+++ application/translations/en/custom_lang.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ *    This file is part of OXID eShop Community Edition.
+ *
+ *    OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    OXID eShop Community Edition is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @package   lang
+ * @copyright (C) OXID eSales AG 2003-2012
+ * @version OXID eShop CE
+ * @translators: marco (3357)
+ */
+
+$sLangName  = 'American English';
+
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = array(
+
+
+
+
+'ALREADY_CUSTOMER'   										=> 'I am already a customer',
+'ART_NUMBER' 												=> 'Prod. No.:',
+'BACK_TO_START_PAGE' 										=> 'back to Start Page',
+'BASKET_TOTAL_GIFTCARD_COSTS' 								=> 'Total Gift Card Cost(s)',
+'BASKET_TOTAL_GIFTCARD_COSTS_NET' 							=> 'Net Gift Card Cost(s)',
+'BASKET_TOTAL_PLUS_PROPORTIONAL_VAT' 						=> 'plus TAX (proportionally calculated)',
+'BASKET_TOTAL_PLUS_VAT' 									=> 'plus TAX',
+'BASKET_TOTAL_WRAPPING_COSTS' 								=> 'Total Gift Wrapping',
+'BASKET_TOTAL_WRAPPING_COSTS_NET' 							=> 'Net Gift Wrapping',
+'BILLING_SHIPPING_SETTINGS' 								=> 'Billing and Shipping Address Settings',
+'BIRTHDATE' 												=> 'Birth Date:',
+'CARD_MASTERCARD' 											=> 'MasterCard',
+'CELLUAR_PHONE' 											=> 'Cellular Phone:',
+'COOKIE_NOTE' => 'This online shop is using cookies to give you the best possible shopping experience. Therefore session information and\/or language settings are stored on your computer. Without cookies the range of the online shop\'s functionality is limited.',
+'EACH'														=> 'each'
+'ERROR_MESSAGE_ACCESSRIGHT_ACCESSDENIED' 					=> 'Access denied, insufficient rights!',
+'ERROR_MESSAGE_ARTICLE_NOPRODUCTID' 						=> 'No product ID given!',
+'ERROR_MESSAGE_CHECK_EMAIL' 								=> 'An error occurred sending the e-mail - please check the e-mail address.',
+'ERROR_MESSAGE_ERROR_OCCURED' 								=> 'An OXID eShop error occurred!',
+'ERROR_MESSAGE_NOTALLOWEDTYPE' 								=> 'File type not allowed (please, edit config.inc.php to allow this type of file)',
+'ERROR_MESSAGE_UNLICENSED1' 								=> 'This shop is unlicensed, please contact',
+'ERROR_MESSAGE_USER_USEREXISTS' 							=> 'Not possible to register %s. Maybe you have registered previously?',
+'ERROR_MESSAGE_VERSION_EXPIRED1' 							=> 'Your OXID version is expired, please contact',
+'ERROR_MESSAGE_VOUCHER_INCORRECTPRICE' 						=> 'Purchase price is too low for using this coupon!',
+'ERROR_MESSAGE_VOUCHER_NOTVALIDUSERGROUP' 					=> 'This coupon is not valid for your user group!',
+'ERROR_MESSAGE_VOUCHER_TOTALBELOWZERO' 						=> 'Purchase price below \$0 is not allowed!',
+'FACEBOOK_ENABLE_INFOTEXT' 									=> 'Here you can leave information about the transmission of personal data to Facebook.<br><br>While using Facebook plug-ins, a connection is established enabling the sharing of content with other users. When using plug-ins while browsing through the online shop and being logged in on Facebook at the same time, your personal data is automatically transmitted to Facebook. You can find more information about collecting, using and processing of your personal data on <a href="http://www.facebook.com/policy.php" target="_blank">Facebook\'s Privacy Policy</a>.',
+'FACEBOOK_INVITETEXT' 										=> 'Check this out. Simply click on the button below to see my recommended product.',
+'fullDateFormat' 											=> 'm-d-Y H:i:s',
+'GIFT_REGISTRY_SEARCH_RESULTS' 								=> 'Gift Registry search results:',
+'INCL_TAX_AND_PLUS_SHIPPING' 								=> '* All prices exclude TAX and shipping',
+'MESSAGE_COUPON_ACCUMULATION_DIFFERENT_SERIE' 				=> 'Duplication with Coupon of different series not permitted.',
+'MESSAGE_COUPON_NOT_APPLIED_FOR_ARTICLES' 					=> 'Discounts are not applied to these products',
+'MESSAGE_DOWNLOADABLE_PRODUCT' 								=> 'Hint: You have downloadable products in your shopping cart. If you buy without registering, you will have product download links only in your order e-mail. If you are registered, links will be displayed in My Account -> My Downloads section.',
+'MESSAGE_GET_BONUS_POINTS' 									=> 'Now, get your bonus points for your purchase!',
+'MESSAGE_INVITE_YOUR_FRIENDS' 								=> 'Invite your friends to our site and get bonus points for each invited user.',
+'MESSAGE_INVITE_YOUR_FRIENDS_EMAIL' 						=> 'Please enter your friend\'s e-mail address below and an invitation letter will be sent to them.',
+'MESSAGE_INVITE_YOUR_FRIENDS_INVITATION_SENT' 				=> 'An invitation e-mail was sent to your friends. Thank you for inviting your friends.',
+'MESSAGE_LOGIN_TO_RATE' 									=> 'You have to be logged in to enter a rating!',
+'MESSAGE_MAKE_GIFT_REGISTRY_PUBLISH' 						=> 'Everyone will be able to search and display my gift registry:',
+'MESSAGE_MY_DOWNLOADS_LINK_EXPIRED' 						=> 'Download link has been reactivated, try it now.',
+'MESSAGE_NEWSLETTER_SENT_CONFIRMATION' 						=> 'You have just been sent a subscription activation e-mail.',
+'MESSAGE_NEWSLETTER_SUBSCRIPTION' 							=> 'You can cancel newsletter at any time.',
+'MESSAGE_NEWSLETTER_SUBSCRIPTION_ACTIVATED' 				=> 'Your Newsletter subscription has been activated.',
+'MESSAGE_NEWSLETTER_SUBSCRIPTION_CENCELED' 					=> 'Your Newsletter subscription has been canceled.',
+'MESSAGE_NEWSLETTER_SUBSCRIPTION_SUCCESS' 					=> 'Your Newsletter subscription was successful.',
+'MESSAGE_NO_DELIVERY_METHOD' 								=> '<p>We have no delivery carrier set up for this country.</p> <p>We are working to find a possible delivery carrier and will e-mail new information including costs to you as soon as possible.</p> <p>If you agree with this procedure, please select "CONTINUE TO NEXT STEP"</p>',
+'MESSAGE_PLEASE_DELETE_DIRECTORY_SETUP' 					=> 'setup.',
+'MESSAGE_PLEASE_DELETE_DIRECTORY_UPDATEAPP' 				=> 'update App',
+'MESSAGE_PRICE_ALARM_PRICE_CHANGE' 							=> 'We will be happy to inform you when a price change of this product hits the price at which you want to purchase.',
+'MESSAGE_SELECTED_IPAYMENT' 								=> 'You selected "iPayment" as your Payment Method.<br>Please enter all your payment data in the browser-window, which is going to be opened.<br><br>In case the iPayment browser-window doesn\`t open automatically (e.g., because you deactivated Javascript in your browser), please click on the iPayment button below.',
+'MESSAGE_SEND_GIFT_REGISTRY' 								=> 'Click here to send your Gift Registry to your friends.',
+'MESSAGE_STOCK_LOW' 										=> 'Stock low: This Product has reached the specified low stock limit.',
+'MESSAGE_THANKYOU_FOR_SUBSCRIBING_NEWSLETTERS' 				=> 'Thank you for subscribing to our newsletter.',
+'MESSAGE_TO_BE_LOGGED_WRITE_GUESTBOOK' 						=> 'You must be logged in to write a guestbook entry.',
+'MESSAGE_UNAVAILABLE_SHIPPING_METHOD' 						=> 'The shipping method you selected is no longer available. Please choose another shipping method.',
+'MESSAGE_WELCOME_REGISTERED_USER' 							=> 'We welcome you as a new registered user!',
+'MESSAGE_YOU_RECEIVED_ORDER_CONFIRM' 						=> 'You already received an order confirmation via e-mail.',
+'MY_WISHLIST_BY' 											=> 'My Wish List by',
+'MY_WISH_LIST' 												=> 'My Wish List',
+'NEWEST_SHOP_PRODUCTS' 										=> 'NEWEST SHOP PRODUCTS',
+'NEWEST_SHOP_PRODUCTS_DESCRIPTION' 							=> 'Newest Shop Products',
+'NOTICELIST' 												=> 'Notice List',
+'OXBUNDLEID' 												=> 'Bundle Ident. No.',
+'OXID' 														=> 'Internal Ident. No.',
+'OXNOSTOCKTEXT' 											=> 'Out-Of-Stock Message',
+'OXPARENTID' 												=> 'Ident. No. Main Product',
+'OXSEOID' 													=> 'SEO ID',
+'OXSHOPID' 													=> 'Shop Ident. No.',
+'OXSTOCKTEXT' 												=> 'In Stock Message',
+'OXVAT' 													=> 'Product TAX',
+'OXVPE' 													=> 'Packaging unit',
+'PLUS_SHIPPING' 											=> 'plus',
+'POSTAL_CODE' 												=> 'Zipcode',
+'POSTAL_CODE_AND_CITY' 										=> 'Zipcode, City:',
+'PRICE_ALERT_FOR_PRODUCT' 									=> 'Price alert for product',
+'PRODUCT_IMAGES' 											=> 'Product Images',
+'REDUCED_FROM_2' 											=> 'MSRP',
+'REGISTRATION_DESCRIPTION' 									=> 'Customers with an account have advantages like:<br> - Administration of shipping addresses<br> - Check order status<br> - Order History<br> - Personal Wish List<br> - Personal Gift Registry<br> - Newsletter subscription<br> - Special offers and discounts',
+'REQUEST_PASSWORD_AFTERCLICK' 								=> 'When you click on \'Request Password\', you\'ll be sent an e-mail with instructions on how to set up a new password.',
+'SET'														=> 'set',
+'SHIPPING_VAT1' 											=> 'plus TAX',
+'STOCK_LOW' 												=> 'Low Stock',
+'STREET_AND_STREETNO' 										=> 'Street, Street No.:',
+'SUBSCRIBE_NEWSLETTER_MESSAGE' 								=> 'You can cancel newsletter at any time.',
+'TOP_SHOP_PRODUCTS' 										=> 'Top shop products',
+'TO_MY_WISHLIST' 											=> 'To go to my Wish List',
+'USED_COUPONS' 												=> 'The following Coupons are used',
+'WHO_BOUGHT_ALSO_BOUGHT' 									=> 'Customers who bought this product, also bought:',
+'WISHLIST_BY' 												=> 'Wish List by',
+'WRAPPING_DESCRIPTION' 										=> 'We\'re happy to wrap your gift, add a gift card and/or greeting with your personal message.',
+?>
