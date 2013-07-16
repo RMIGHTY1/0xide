67fb8424cf6b70ae2a9dba1cf8a80960bb174b6a
Ray Snarski <bibletruth@absolutelyright.org>
2013-07-16 18:21:57 -0500

    Create cust_lang.php
    Americanized. Also works with Registration page modifications listed in this github/rmighty1.

---
+++ application/views/egazure/en/cust_lang.php
@@ -0,0 +1,67 @@
+<?php
+$sLangName  = 'English';
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+
+$aLang = array(
+
+'charset'                                                     => 'ISO-8859-15',
+
+// Overrides
+'ADDITIONAL_INFO'                                             => 'Address Line 2:',
+'ALREADY_CUSTOMER'                                            => 'I am already a customer',
+'CONTACT'                                                     => 'Contact Us',
+'CONTACTS'                                                    => 'Contact Us',
+'ERROR_MESSAGE_ERROR_OCCURED'                                 => 'An internal error occured!',
+'ERROR_MESSAGE_MANDATES_EXCEEDED2'                            => 'Oxid Commerce AG',
+'ERROR_MESSAGE_OXID_SHOP_ERROR'                               => 'Internal Error',
+'ERROR_MESSAGE_OXID_SHOP_ERROR2'                              => 'Internal ERROR!',
+'ERROR_MESSAGE_UNLICENSED2'                                   => 'Oxid Commerce AG',
+'ERROR_MESSAGE_VERSION_EXPIRED1'                              => 'Your eShop version is expired, contact',
+'ERROR_MESSAGE_VERSION_EXPIRED2'                              => 'Oxid Commerce AG',
+'FORM_FIELDSET_USER_BILLING_COMPANY'                          => 'Company / Nursing Home:',
+'FORM_FIELDSET_USER_BILLING_PHONE'                            => 'Daytime Phone No.',
+'FORM_FIELDSET_SALUTATION_SALUTATION_MR'                      => 'Mr.',
+'FORM_FIELDSET_SALUTATION_SALUTATION_MRS'                     => 'Mrs.',
+'FORM_FIELDSET_SALUTATION_SALUTATION_MS'                      => 'Ms.',
+'FORM_FIELDSET_SALUTATION_SALUTATION_DR'                      => 'Dr.',
+'FORM_FIELDSET_SALUTATION_SALUTATION_PROF'                    => 'Prof.',
+'FORM_FIELDSET_USER_BILLING_STREET'                           => 'Address Line 1:',
+'FORM_FIELDSET_USER_BILLING_TITLE'                            => 'Title:',
+'FORM_FIELDSET_USER_BILLING_CITY'                             => 'City:',
+'FORM_FIELDSET_USER_BILLING_POSTALCODE'                       => 'Zip Code:',
+'FORM_FIELDSET_USER_BILLING_CITYANDPOSTALCODE'                => 'City, Zip Code:',
+'FORM_FIELDSET_USER_BILLING_POSTALCODEANDCITY'                => 'City, Zip Code:',
+'GRAND_TOTAL'                                                 => 'Grand Total',
+'INCL_TAX_AND_PLUS_SHIPPING'  		              => '* Taxes included in all prices',
+'MESSAGE_NOT_ON_STOCK'                                        => 'Sorry, this item is out of stock and<br> cannot be ordered at the moment.',
+'MESSAGE_PLEASE_CONTACT_SUPPORT'                              => 'Please contact Oxid Commerce support staff.',
+'MESSAGE_YOU_RECEIVED_ORDER_CONFIRM'                          => 'An order confirmation has been sent to your e-mail.',
+'NOTICELIST'                                                  => 'My Wish List',
+'OXID_ESALES_URL'                                             => 'http://www.oxid-esales.com/en/home.html/',
+'OXID_ESALES_URL_TITLE'                                       => 'eCommerce software by Oxid Commerce AG',
+'PAGE_ACCOUNT_DASHBOARD_COMPANY'                              => 'Company / Nursing Home:',
+'PHONE' 				                      => 'Daytime Phone No.',
+'POSTAL_CODE_AND_CITY'                                        => 'City, Zip Code:',
+'PLEASE_SELECT_STATE'                                         => 'Select your state',
+'PLUS_SHIPPING'                                               => 'incl. tax, plus ',
+'READY'                                                       => 'Done!',
+'REDUCED_FROM_2'                                              => '',
+'SHIPPING_COST'                                               => 'Shipping & Handling',
+'STEPS_BASKET'                                                => 'Cart',
+'STEPS_ORDER'                                                 => 'Order',
+'STEPS_PAY'                                                   => 'Payment',//'Options',
+'STEPS_SEND'                                                  => 'Address',
+'STREET_AND_STREETNO'                                         => 'Address Line 1:',
+'TOTAL_GROSS'                                                 => 'Cart Subtotal',
+'WHO_BOUGHT_ALSO_BOUGHT'                                      => 'Customers who bought these products, also bought:',
+'WIDGET_TWITTER_FEED_HEADER'                                  => 'Twitter Feed',
+
+
+
+/* Dashboard Modification   */
+'PAGE_ACCOUNT_DASHBOARD_COMPANY'                              => 'Company',
+'PAGE_ACCOUNT_DASHBOARD_ADDRESS'                              => 'Address',
+'PAGE_ACCOUNT_DASHBOARD_PHONE'                                => 'Phone Number',
+);
